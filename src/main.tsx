62ece77fa6953c2bb53a01a56b1e3c7e5f95f840
BladerX11 <BladerX11@proton.me>
2026-03-26 00:28:31 +0800

    feat: create context for shared settings

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import App from './App.tsx';
+import { SettingsProvider } from './context/SettingsContext.tsx';
 import '@mantine/core/styles.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider defaultColorScheme="dark">
-      <App />
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
     </MantineProvider>
   </StrictMode>
 );
