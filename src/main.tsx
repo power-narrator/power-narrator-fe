d7912982e217a700b4076df614ecba05e2e63ee1
BladerX11 <BladerX11@proton.me>
2026-03-26 00:28:31 +0800

    refactor: cleanup unused dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import App from './App.tsx';
 import '@mantine/core/styles.css';
-import '@mantine/dropzone/styles.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
