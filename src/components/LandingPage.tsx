3107d9eb860c9386ba8e867927bcc341c61513e3
BladerX11 <BladerX11@proton.me>
2026-04-04 18:02:38 +0800

    refactor: remove repeated root height and width styles

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { Text, Button } from "@mantine/core";
+import { Button, Stack, Text } from "@mantine/core";
 
 interface LandingPageProps {
   onSelectFile?: () => void;
@@ -6,22 +6,11 @@ interface LandingPageProps {
 
 export function LandingPage({ onSelectFile }: LandingPageProps) {
   return (
-    <div
-      style={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
+    <Stack>
       <Button onClick={onSelectFile} size="xl" variant="filled" color="blue">
         Select PowerPoint File
       </Button>
-      <Text c="dimmed" mt="md">
-        Select a .pptx file to begin
-      </Text>
-    </div>
+      <Text c="dimmed">Select a .pptx file to begin</Text>
+    </Stack>
   );
 }
